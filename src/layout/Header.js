f025f8d783060d34ebc91cf9fa0fb54f28a82d66
zsoltMeszaros <meszazaolt13@gmail.com>
2020-05-11 10:59:23 +0200

    Login/Registed buttons added

--- src/layout/Header.js
+++ src/layout/Header.js
@@ -1,22 +1,33 @@
 import React from 'react';
 import {Link, Toolbar, Typography, Container, Box, Button} from '@material-ui/core';
+import ButtonGroup from "@material-ui/core/ButtonGroup";
 
 
-const Header = (props) => {
+const Header = () => {
 
     return (
         <Box style={headerStyle}>
             <Container style={toolbarStyle} maxWidth={"lg"}>
-                        <Link style={{textDecoration: "none",}} color="inherit" href={"/"}>
-                            <Typography variant="h4">
-                                Forktastic
-                            </Typography>
-                        </Link>
-                        <Link href={"/random"} style={{textDecoration: "none", marginLeft: "auto"}}>
-                            <Button variant={"contained"} color={"secondary"}>
-                                Random Recipe
-                            </Button>
-                        </Link>
+                <Link style={{textDecoration: "none",}} color="inherit" href={"/"}>
+                    <Typography variant="h4">
+                        Forktastic
+                    </Typography>
+                </Link>
+                <Container style={{ width: "fit-content", marginRight: "0px" }}>
+                    <Link href={"/random"} style={{textDecoration: "none", }}>
+                        <Button variant={"contained"} color={"secondary"}>
+                            Random Recipe
+                        </Button>
+                    </Link>
+                    <ButtonGroup style={{textDecoration: "none", marginLeft: "20px"}}>
+                        <Button variant={"contained"} color={"primary"}>
+                            Register
+                        </Button>
+                        <Button variant={"contained"} color={"primary"}>
+                            Login
+                        </Button>
+                    </ButtonGroup>
+                </Container>
             </Container>
         </Box>
     );
